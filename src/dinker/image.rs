5b016657b91848fd2ff168111a7ab08023a89c0a
andrew <>
2023-02-10 00:08:20 +0900

    Version updates

--- src/dinker/image.rs
+++ src/dinker/image.rs
@@ -59,7 +59,7 @@ impl Image {
         &self.0.shared
     }
 
-    pub fn depends_on(self, dep: &impl Resource) -> Self {
+    pub fn depends_on(self, dep: &impl Dependable) -> Self {
         self.0.data.borrow_mut().depends_on.push(dep.extract_ref());
         self
     }
@@ -287,6 +287,12 @@ impl Image {
 impl Resource for Image {
     fn extract_ref(&self) -> String {
         format!("{}.{}", self.0.extract_resource_type(), self.0.extract_tf_id())
+    }
+}
+
+impl Dependable for Image {
+    fn extract_ref(&self) -> String {
+        Resource::extract_ref(self)
     }
 }
 
